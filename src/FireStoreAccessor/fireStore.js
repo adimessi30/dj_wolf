57bafdcbf1b7fdaf0ffb95dffe58d12d37916e15
Adithya Aiyaswami <aiyasa@amazon.com>
2020-10-30 22:58:16 +0530

    Moving firestore config to untracked file

--- src/FireStoreAccessor/fireStore.js
+++ src/FireStoreAccessor/fireStore.js
@@ -1,16 +1,8 @@
 import { initializeApp } from 'firebase/app'
 import 'firebase/firestore'
+import { config } from './fireStoreConfig'
 
-const firebaseApp = initializeApp({
-  apiKey: 'AIzaSyAagx22Lh99_cRftsQC9cz1SaA_CVIUPvs',
-  authDomain: 'dj-wolf-e8e5f.firebaseapp.com',
-  databaseURL: 'https://dj-wolf-e8e5f.firebaseio.com',
-  projectId: 'dj-wolf-e8e5f',
-  storageBucket: 'dj-wolf-e8e5f.appspot.com',
-  messagingSenderId: '44240032766',
-  appId: '1:44240032766:web:eeb529b000cbc586d1e6af',
-  measurementId: 'G-945H6J20N5'
-})
+const firebaseApp = initializeApp(config)
 
 const fireStoreDB = firebaseApp.firestore()
 
